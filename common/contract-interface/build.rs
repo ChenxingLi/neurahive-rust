59016653ab0e6a3e6d5203acca05af9443e21bfd
Chenxing Li <ChenxingLi@users.noreply.github.com>
2023-08-17 15:03:29 +0800

    Check prerequisites in build.rs

--- common/contract-interface/build.rs
+++ common/contract-interface/build.rs
@@ -5,10 +5,33 @@ const INSTALL_ERROR_MESSAGE: &str =
 const COMPILE_ERROR_MESSAGE: &str =
     "Compile solidity contracts fail, try to run `yarn compile` in folder 'neurahive-contracts'";
 
+fn check_prerequisites() {
+    // Check if node is installed
+    let output = Command::new("node")
+        .arg("-v")
+        .output()
+        .expect("Node is not installed or not found in PATH");
+
+    let version = String::from_utf8(output.stdout).expect("Failed to parse node version");
+    let major_version: i32 = version.split('.').next().unwrap_or("v0")[1..].parse().expect("Failed to parse major version");
+    if major_version != 16 {
+        panic!("Node v16 is required, but current version is: {}", version);
+    }
+
+    // Check if yarn is installed
+    Command::new("yarn")
+        .arg("--version")
+        .output()
+        .expect("Yarn is not installed or not found in PATH");
+}
+
+
 fn main() {
     if cfg!(feature = "compile-contracts") {
         println!("cargo:rerun-if-changed=../../neurahive-contracts/contracts/");
         println!("cargo:rerun-if-changed=../../neurahive-contracts/hardhat.config.ts");
+
+        check_prerequisites();
 
         let output = Command::new("yarn")
             .arg("--cwd")
--- node/build.rs
+++ node/build.rs
@@ -1,7 +1,18 @@
 use std::process::Command;
 
+fn check_prerequisites() {
+    const ERROR_MESSAGE: &str = "Go is not installed or not found in PATH";
+    let output = Command::new("go")
+        .arg("version")
+        .output()
+        .expect(ERROR_MESSAGE);
+    assert!(output.status.success(), "{}", ERROR_MESSAGE);
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=../neurahive-client");
+
+    check_prerequisites();
 
     let status = Command::new("go")
         .current_dir("../neurahive-client")
